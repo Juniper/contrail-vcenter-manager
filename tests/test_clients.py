98a9bd41c4dd29eb47194613af6348308debd1a6
Adrian Szczepański <adrian.szczepanski@codilime.com>
2018-04-12 12:00:37 +0200

    Add method for setting VLAN IDs

--- tests/test_clients.py
+++ tests/test_clients.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 from mock import Mock, patch
 
-from cvm.clients import ESXiAPIClient
+from cvm.clients import ESXiAPIClient, VCenterAPIClient, make_dv_port_spec
 from tests.test_services import create_vmware_vm_mock
 
 
@@ -24,3 +24,35 @@ class TestESXiAPIClient(TestCase):
         result = self.esxi_client.read_vm_properties(self.vmware_vm)
 
         self.assertEqual('VM', result.get('name'))
+
+
+class TestVCenterAPIClient(TestCase):
+    def setUp(self):
+        self.vcenter_client = VCenterAPIClient({})
+
+    def test_set_vlan_id(self):
+        dv_port = Mock(key='8')
+        dv_port.config.configVersion = '1'
+        dvs = Mock()
+        dvs.FetchDVPorts.return_value = [dv_port]
+        with patch('cvm.clients.SmartConnectNoSSL'):
+            with self.vcenter_client:
+                self.vcenter_client.set_vlan_id(dvs=dvs, key='8', vlan_id=10)
+
+        dvs.ReconfigureDVPort_Task.assert_called_once()
+        spec = dvs.ReconfigureDVPort_Task.call_args[1].get('port', [None])[0]
+        self.assertIsNotNone(spec)
+        self.assertEqual('8', spec.key)
+        self.assertEqual('1', spec.configVersion)
+        self.assertEqual(10, spec.setting.vlan.vlanId)
+
+
+class TestFunctions(TestCase):
+    def test_make_dv_port_spec(self):
+        dv_port = Mock(key='8')
+        dv_port.config.configVersion = '1'
+        spec = make_dv_port_spec(dv_port, 10)
+        self.assertEqual('8', spec.key)
+        self.assertEqual('edit', spec.operation)
+        self.assertEqual(10, spec.setting.vlan.vlanId)
+        self.assertEqual('1', spec.configVersion)
