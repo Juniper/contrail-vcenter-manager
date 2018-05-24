f6ee5021bfbe2a49bd22667e0ebc5489d923bba8
Magda Zaremba <magdalena.zaremba@codilime.com>
2018-05-24 15:38:40 +0200

    Added setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+def requirements(filename):
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    return lines
+
+setup(
+    name="contrail-vcenter-manager",
+    version="0.1dev",
+    packages=find_packages(),
+    zip_safe=False,
+    long_description="Contrail vCenter Manager",
+    install_requires=requirements('requirements.txt'),
+)
