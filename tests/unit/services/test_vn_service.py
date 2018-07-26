03f1c02008fc95d2670e37d4c4105ac467a757cc
Adrian Szczepański <adrian.szczepanski@codilime.com>
2018-07-26 14:57:47 +0200

    VLAN trunks for portgroups
    Check reserved ports for one host only
    Sync all VNC VNs on startup
    
    Change-Id: I8750ed3430fe324c81e291fd2deda82705864093
    Closes-Bug: #1783768

--- tests/unit/services/test_vn_service.py
+++ tests/unit/services/test_vn_service.py
@@ -28,7 +28,27 @@ def test_update_vns(vn_service, database, vcenter_api_client, vnc_api_client, vm
         vnc_vn=vnc_vn_1,
         vmware_vn=portgroup,
     )
-    vcenter_api_client.get_dpg_by_key.called_once_with('dvportgroup-1')
-    vcenter_api_client.enable_vlan_override.called_once_with(portgroup)
+    vcenter_api_client.get_dpg_by_key.assert_called_once_with('dvportgroup-1')
+    vcenter_api_client.enable_vlan_override.assert_called_once_with(portgroup)
+    vcenter_api_client.set_vlan_trunk.assert_called_once_with(portgroup)
     fq_name = [VNC_ROOT_DOMAIN, VNC_VCENTER_PROJECT, 'DPG1']
-    vnc_api_client.read_vn.called_once_with(fq_name)
+    vnc_api_client.read_vn.assert_called_once_with(fq_name)
+
+
+def test_sync_vns(vn_service, database, vcenter_api_client, vnc_api_client, vnc_vn_1, portgroup):
+    vnc_api_client.get_vns_by_project.return_value = [vnc_vn_1]
+    vcenter_api_client.get_dpg_by_name.return_value = portgroup
+
+    vn_service.sync_vns()
+
+    vn_model = database.get_vn_model_by_key('dvportgroup-1')
+    assert vn_model is not None
+    assert_vn_model_state(
+        vn_model,
+        key='dvportgroup-1',
+        vnc_vn=vnc_vn_1,
+        vmware_vn=portgroup,
+    )
+
+    vcenter_api_client.enable_vlan_override.assert_called_once_with(portgroup)
+    vcenter_api_client.set_vlan_trunk.assert_called_once_with(portgroup)
