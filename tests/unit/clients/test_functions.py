5e7d9b16f78aff0987bc5ec3f4921fc442be4b92
Adrian Szczepański <adrian.szczepanski@codilime.com>
2018-09-13 12:01:07 +0200

    Fix Security Group creation
    Change sg name in AddressType creation
    
    Change-Id: I39bf322b7871a08342df066d152d9d01ce22a487
    Closes-Bug: #1792350

--- tests/unit/clients/test_functions.py
+++ tests/unit/clients/test_functions.py
@@ -1,4 +1,5 @@
-from cvm.clients import make_dv_port_spec
+from cvm.clients import construct_security_group, make_dv_port_spec
+from cvm.constants import VNC_VCENTER_DEFAULT_SG, VNC_VCENTER_DEFAULT_SG_FQN
 
 
 def test_make_dv_port_spec(dv_port):
@@ -18,3 +19,16 @@ def test_make_port_spec_restore(dv_port):
     assert spec.operation == 'edit'
     assert spec.setting.vlan.inherited is True
     assert spec.configVersion == '1'
+
+
+def test_construct_security_group(project):
+    sg = construct_security_group(project)
+
+    assert sg.name == VNC_VCENTER_DEFAULT_SG
+
+    sg_fqn = ':'.join(VNC_VCENTER_DEFAULT_SG_FQN)
+    assert sg.security_group_entries.policy_rule[0].src_addresses[0].security_group == sg_fqn
+    assert sg.security_group_entries.policy_rule[0].dst_addresses[0].security_group == 'local'
+
+    assert sg.security_group_entries.policy_rule[1].src_addresses[0].security_group == 'local'
+    assert sg.security_group_entries.policy_rule[1].dst_addresses[0].subnet.ip_prefix == '0.0.0.0'
