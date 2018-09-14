2395a76266b6a1c0aa5928a195f255bd012f2ddd
Krzysztof Gromadzki <krzysztof.gromadzki@codilime.com>
2018-09-14 14:12:45 +0200

    Revert "Main file refactor"
    This reverts commit 7e53f34b9ffb36cb5adb4d8a594146e62d50a8b0.
    
    Change-Id: Ic332e37c3de9462c32dac81bc370b313460935be
    Partial-Bug: #1791035

--- tests/unit/monitors/test_monitors.py
+++ tests/unit/monitors/test_monitors.py
@@ -2,7 +2,6 @@
 import pytest
 from mock import Mock
 
-from cvm.constants import EVENTS_TO_OBSERVE
 from cvm.monitors import VMwareMonitor
 
 
@@ -27,14 +26,3 @@ def test_pass_update_to_controller(monitor, controller, esxi_api_client, vm_crea
         pass
 
     controller.handle_update.assert_called_once_with(vm_created_update)
-
-
-def test_configure_esxi_client(esxi_api_client, controller):
-    ehc = Mock()
-    esxi_api_client.create_event_history_collector.return_value = ehc
-
-    VMwareMonitor(esxi_api_client, controller)
-
-    esxi_api_client.create_event_history_collector.assert_called_once_with(EVENTS_TO_OBSERVE)
-    esxi_api_client.add_filter.assert_called_once_with(ehc, ['latestPage'])
-    esxi_api_client.make_wait_options.assert_called_once_with(120)
