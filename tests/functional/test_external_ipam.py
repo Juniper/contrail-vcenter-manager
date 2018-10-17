b9a27b01607dc3fbb1c5f1a8297b6360f7e578ed
Krzysztof Gromadzki <krzysztof.gromadzki@codilime.com>
2018-10-17 15:03:22 +0200

    Fix Instance IP and IP Address management
    Fix updating VMIs VN and Instance IP in VNC
    Fix VMI Models _ip_address field management
    More verbose logging for Instance IP handling
    
    Closes-Bug: #1797318
    Change-Id: If1848f0c71926aa12b90dae470d836851bf07fb9

--- tests/functional/test_external_ipam.py
+++ tests/functional/test_external_ipam.py
@@ -1,13 +1,8 @@
 def test_external_ipam(controller, database, vnc_api_client, vrouter_api_client, vm_created_update, nic_info_update,
                        vn_model_1):
-    """ We don't need ContrailVM model for CVM to operate properly. """
-
     # The network we use uses external IPAM
     vn_model_1.vnc_vn.external_ipam = True
     database.save(vn_model_1)
-
-    # We use external IPAM, so there's no assignment of IP addresses
-    vnc_api_client.create_and_read_instance_ip.side_effect = lambda x: x
 
     controller.handle_update(vm_created_update)
 
@@ -21,11 +16,37 @@ def test_external_ipam(controller, database, vnc_api_client, vrouter_api_client,
     # IP address is updated
     assert vmi_model.ip_address == '192.168.100.5'
     assert vmi_model.vnc_instance_ip.instance_ip_address == '192.168.100.5'
-    assert vnc_api_client.create_and_read_instance_ip.call_args[0][0] is vmi_model.vnc_instance_ip
+    assert vnc_api_client.create_and_read_instance_ip.call_args[0][0] is vmi_model
 
     # vRouter port should not be updated - it will gather IP info from the Controller
     vrouter_api_client.add_port.assert_called_once()
     vrouter_api_client.delete_port.assert_called_once()
 
     # The VMI itself should not be updated, since there's no new info
+    # (one call is from VmCreated)
     vnc_api_client.update_vmi.assert_called_once()
+
+
+def test_no_external_ipam(controller, database, vnc_api_client, vrouter_api_client, nic_info_update,
+                          vn_model_1, vmi_model):
+    # The network we use doesn't use external IPAM
+    vn_model_1.vnc_vn.external_ipam = None
+    database.save(vn_model_1)
+    database.save(vmi_model)
+
+    assert vmi_model.ip_address is None
+    assert vmi_model.vnc_instance_ip is None
+
+    controller.handle_update(nic_info_update)
+
+    # IP address was not updated
+    assert vmi_model.ip_address is None
+    assert vmi_model.vnc_instance_ip is None
+    vnc_api_client.create_and_read_instance_ip.assert_not_called()
+
+    # vRouter port should not be updated - nothing changed
+    vrouter_api_client.add_port.assert_not_called()
+    vrouter_api_client.delete_port.assert_not_called()
+
+    # The VMI itself should not be updated, since there's no new info
+    vnc_api_client.update_vmi.assert_not_called()
