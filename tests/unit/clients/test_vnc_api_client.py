6730d39cc6da64f5917fce6f7148ff72b8b51369
Krzysztof Gromadzki <krzysztof.gromadzki@codilime.com>
2018-10-17 15:03:22 +0200

    Fix Instance IP and IP Address management
    Fix updating VMIs VN and Instance IP in VNC
    Fix VMI Models _ip_address field management
    More verbose logging for Instance IP handling
    
    Closes-Bug: #1797318
    Change-Id: If1848f0c71926aa12b90dae470d836851bf07fb9

--- tests/unit/clients/test_vnc_api_client.py
+++ tests/unit/clients/test_vnc_api_client.py
@@ -2,7 +2,7 @@ from vnc_api.exceptions import NoIdError
 
 
 def test_update_create_vm(vnc_api_client, vnc_lib, vnc_vm):
-    vnc_api_client.update_or_create_vm(vnc_vm)
+    vnc_api_client.update_vm(vnc_vm)
 
     vnc_lib.virtual_machine_update.assert_called_once_with(vnc_vm)
 
@@ -10,38 +10,51 @@ def test_update_create_vm(vnc_api_client, vnc_lib, vnc_vm):
 def test_update_create_new_vm(vnc_api_client, vnc_lib, vnc_vm):
     vnc_lib.virtual_machine_update.side_effect = NoIdError(None)
 
-    vnc_api_client.update_or_create_vm(vnc_vm)
+    vnc_api_client.update_vm(vnc_vm)
 
     vnc_lib.virtual_machine_create.called_once_with(vnc_vm)
 
 
-def test_update_vmi(vnc_api_client, vnc_lib, vnc_vmi):
-    vnc_lib.virtual_machine_interface_read.return_value = vnc_vmi
-    vnc_vmi.get_instance_ip_back_refs.return_value = [{'to': 'instance-ip-fqname'}]
+def test_update_vmi(vnc_api_client, vnc_lib, vnc_vmi_1):
+    vnc_lib.virtual_machine_interface_read.return_value = vnc_vmi_1
+    vnc_vmi_1.get_instance_ip_back_refs.return_value = [{'to': 'instance-ip-fqname'}]
 
-    vnc_api_client.update_vmi(vnc_vmi)
+    vnc_api_client.update_vmi(vnc_vmi_1)
 
-    vnc_lib.virtual_machine_interface_update.assert_called_once_with(vnc_vmi)
-
-
-def test_update_create_new_vmi(vnc_api_client, vnc_lib, vnc_vmi):
-    vnc_lib.virtual_machine_interface_read.side_effect = [NoIdError(None), vnc_vmi]
-    vnc_vmi.get_instance_ip_back_refs.return_value = [{'to': 'instance-ip-fqname'}]
-
-    vnc_api_client.update_vmi(vnc_vmi)
-
-    vnc_lib.virtual_machine_interface_create.assert_called_once_with(vnc_vmi)
+    vnc_lib.virtual_machine_interface_update.assert_called_once_with(vnc_vmi_1)
 
 
-def test_update_vmi_vn(vnc_api_client, vnc_lib, vnc_vmi, vnc_vn):
-    vnc_lib.virtual_machine_interface_read.return_value = vnc_vmi
-    vnc_lib.virtual_network_read.return_value = vnc_vn
-    vnc_vmi.get_instance_ip_back_refs.return_value = [{'to': 'instance-ip-fqname'}]
+def test_update_create_new_vmi(vnc_api_client, vnc_lib, vnc_vmi_1):
+    vnc_lib.virtual_machine_interface_read.side_effect = [NoIdError(None), vnc_vmi_1]
+    vnc_vmi_1.get_instance_ip_back_refs.return_value = [{'to': ['instance-ip-fqname']}]
 
-    vnc_api_client.update_vmi(vnc_vmi)
+    vnc_api_client.update_vmi(vnc_vmi_1)
 
-    vnc_lib.virtual_machine_interface_update.assert_called_once_with(vnc_vmi)
+    vnc_lib.virtual_machine_interface_create.assert_called_once_with(vnc_vmi_1)
+
+
+def test_update_vmi_vn(vnc_api_client, vnc_lib, vnc_vmi_1, vnc_vmi_2, vnc_vn_2):
+    vnc_lib.virtual_machine_interface_read.return_value = vnc_vmi_1
+    vnc_lib.virtual_network_read.return_value = vnc_vn_2
+    vnc_vmi_1.get_instance_ip_back_refs.return_value = [{'to': ['instance-ip-fqname']}]
+
+    vnc_api_client.update_vmi(vnc_vmi_2)
+
+    vnc_vmi_1.set_virtual_network.assert_called_once_with(vnc_vn_2)
+    vnc_lib.virtual_machine_interface_update.assert_called_once_with(vnc_vmi_1)
     vnc_lib.instance_ip_delete.assert_called_once()
+
+
+def test_dont_update_vmi_vn(vnc_api_client, vnc_lib, vnc_vmi_1, vnc_vn_1):
+    vnc_lib.virtual_machine_interface_read.return_value = vnc_vmi_1
+    vnc_lib.virtual_network_read.return_value = vnc_vn_1
+    vnc_vmi_1.get_instance_ip_back_refs.return_value = [{'to': ['instance-ip-fqname']}]
+
+    vnc_api_client.update_vmi(vnc_vmi_1)
+
+    vnc_vmi_1.set_virtual_network.assert_not_called()
+    vnc_lib.virtual_machine_interface_update.assert_called_once_with(vnc_vmi_1)
+    vnc_vmi_1.instance_ip_delete.assert_not_called()
 
 
 def test_get_all_vms(vnc_api_client, vnc_lib, vnc_vm):
