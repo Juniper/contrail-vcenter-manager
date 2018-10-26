38319d5d968c2807355f6495b44cb9227eb01e6d
Adrian <adrian.szczepanski@codilime.com>
2018-10-26 13:50:44 +0200

    Update VLAN ID assignment logic
    Patch sleep in vm removed tests
    Move VLAN ID functionality to separate service
    Prevent freeing the same vlan ID multiple times
    Add is_vlan_available to database
    Update __main__ module
    Update VLAN ID assignment logic
    
    Change-Id: Id20219b7ae91bae5d657ebdf4f705b160a799d5c

--- tests/unit/services/test_vlan_ids.py
+++ tests/unit/services/test_vlan_ids.py
@@ -11,67 +11,62 @@ def prepare_database(database, vm_model, vn_model_1):
     return database
 
 
-def test_sync_vlan_ids(vmi_service, vcenter_api_client, vlan_id_pool):
+def test_sync_vlan_ids(vlan_id_service, vcenter_api_client, vlan_id_pool):
     vcenter_api_client.get_reserved_vlan_ids.return_value = [0, 1]
 
-    vmi_service.sync_vlan_ids()
+    vlan_id_service.sync_vlan_ids()
     new_vlan_id = vlan_id_pool.get_available()
 
     assert new_vlan_id == 2
 
 
 @patch('cvm.services.time.sleep', return_value=None)
-def test_assign_new_vlan_id(_, vmi_service, database, vcenter_api_client,
+def test_assign_new_vlan_id(_, vlan_id_service, database, vcenter_api_client,
                             vlan_id_pool, vmi_model):
-    database.vmis_to_update.append(vmi_model)
+    database.vlans_to_update.append(vmi_model)
     reserve_vlan_ids(vlan_id_pool, [0, 1])
     vcenter_api_client.get_vlan_id.return_value = None
 
-    vmi_service.update_vmis()
+    vlan_id_service.update_vlan_ids()
 
     assert vmi_model.vcenter_port.vlan_id == 2
+    assert not database.vlans_to_update
 
 
-def test_retain_old_vlan_id(vmi_service, database, vcenter_api_client, vmi_model):
-    database.vmis_to_update.append(vmi_model)
+def test_retain_old_vlan_id(vlan_id_service, database, vcenter_api_client, vmi_model):
+    database.vlans_to_update.append(vmi_model)
     vcenter_api_client.get_vlan_id.return_value = 20
 
-    vmi_service.update_vmis()
+    vlan_id_service.update_vlan_ids()
 
     assert vmi_model.vcenter_port.vlan_id == 20
+    assert not database.vlans_to_update
 
 
-@patch('cvm.services.time.sleep', return_value=None)
-def test_current_not_available_with_register_event(_, vmi_service, database, vcenter_api_client,
-                                                   vlan_id_pool, vmi_model):
-    database.vmis_to_update.append(vmi_model)
+@patch('cvm.services.time.sleep')
+def test_current_not_available(_, vlan_id_service, database, vcenter_api_client,
+                               vlan_id_pool, vmi_model, vmi_model_2):
+    database.vlans_to_update.append(vmi_model)
+    vmi_model_2.vcenter_port.vlan_id = 20
+    database.save(vmi_model_2)
     vcenter_api_client.get_vlan_id.return_value = 20
     reserve_vlan_ids(vlan_id_pool, [20])
 
-    vmi_service.update_vmis(vm_registered=True)
+    vlan_id_service.update_vlan_ids()
 
     assert vmi_model.vcenter_port.vlan_id == 0
+    assert not database.vlans_to_update
 
 
-def test_current_not_available_without_register_event(vmi_service, database, vcenter_api_client,
-                                                      vlan_id_pool, vmi_model):
-    database.vmis_to_update.append(vmi_model)
-    vcenter_api_client.get_vlan_id.return_value = 20
-    reserve_vlan_ids(vlan_id_pool, [20])
-
-    vmi_service.update_vmis(vm_registered=False)
-
-    assert vmi_model.vcenter_port.vlan_id == 20
-
-
-def test_restore_vlan_id(vmi_service, database, vcenter_api_client,
+def test_restore_vlan_id(vlan_id_service, database, vcenter_api_client,
                          vlan_id_pool, vmi_model):
     reserve_vlan_ids(vlan_id_pool, [20])
-    database.vmis_to_delete.append(vmi_model)
+    database.vlans_to_restore.append(vmi_model)
     vmi_model.vcenter_port.vlan_id = 20
 
-    vmi_service.update_vmis()
+    vlan_id_service.update_vlan_ids()
 
     vcenter_api_client.restore_vlan_id.assert_called_once_with(
         vmi_model.vcenter_port)
     assert vlan_id_pool.is_available(20)
+    assert not database.vlans_to_restore
