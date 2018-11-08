a9c0888238421f059338d096d36a4c5bd79fbeba
Krzysztof Gromadzki <krzysztof.gromadzki@codilime.com>
2018-11-08 12:24:10 +0100

    Add greenlet traceback to CVM Introspect
    Change-Id: I3ea5a54cd390a6bbe769856960d5276e945e24ee
    Partial-Bug: #1800895

--- cvm/sandesh_handler.py
+++ cvm/sandesh_handler.py
@@ -1,3 +1,9 @@
+import gc
+import traceback
+import greenlet
+from cfgm_common.uve.greenlets.ttypes import (GreenletObjectReq,
+                                              GreenletObject,
+                                              GreenletObjectListResp)
 from cvm.sandesh.vcenter_manager.ttypes import (VirtualMachineData,
                                                 VirtualMachineInterfaceData,
                                                 VirtualMachineInterfaceRequest,
@@ -19,6 +25,7 @@ class SandeshHandler(object):
         VirtualMachineRequest.handle_request = self.handle_virtual_machine_request
         VirtualNetworkRequest.handle_request = self.handle_virtual_network_request
         VirtualMachineInterfaceRequest.handle_request = self.handle_virtual_machine_interface_request
+        GreenletObjectReq.handle_request = self.handle_greenlet_obj_list_request
 
     def handle_virtual_machine_request(self, request):
         with self._lock:
@@ -59,6 +66,33 @@ class SandeshHandler(object):
             ]
         response = VirtualMachineInterfaceResponse(virtual_interfaces_data)
         response.response(request.context())
+
+    @classmethod
+    def handle_greenlet_obj_list_request(cls, request):
+        greenlets = []
+        for obj in gc.get_objects():
+            if not isinstance(obj, greenlet.greenlet):
+                continue
+            greenlet_stack = "".join(traceback.format_stack(obj.gr_frame))
+            greenlet_name = cls._get_greenlet_name(obj)
+            greenlets.append(
+                GreenletObject(greenlet_traces=greenlet_stack, greenlet_name=greenlet_name)
+            )
+        if request.greenlet_name is not None:
+            greenlets = [obj for obj in greenlets if obj.greenlet_name == request.greenlet_name]
+        response = GreenletObjectListResp(greenlets=greenlets)
+        response.response(request.context())
+
+    @classmethod
+    def _get_greenlet_name(cls, obj):
+        if not hasattr(obj, 'greenlet_name'):
+            try:
+                greenlet_name = obj._run.__name__
+            except AttributeError:
+                greenlet_name = 'Anonymous'
+        else:
+            greenlet_name = obj.greenlet_name
+        return greenlet_name
 
 
 class SandeshConverter(object):
