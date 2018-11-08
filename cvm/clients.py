54ee25a19f4b5442c2a102a9986494764bfac938
Krzysztof Gromadzki <krzysztof.gromadzki@codilime.com>
2018-11-08 16:36:27 +0100

    Respawing events listening greenlet when connection to ESXi hangs
    Change-Id: Ia9de9a47bd1afe718e35aecf24acbb63f8270d16
    Closes-Bug: #1800895

--- cvm/clients.py
+++ cvm/clients.py
@@ -13,7 +13,7 @@ from vnc_api import vnc_api
 from vnc_api.exceptions import NoIdError, RefsExistError
 
 from contrail_vrouter_api.vrouter_api import ContrailVRouterApi
-from cvm.constants import (VM_PROPERTY_FILTERS, VNC_ROOT_DOMAIN,
+from cvm.constants import (ID_PERMS_CREATOR, VM_PROPERTY_FILTERS, VNC_ROOT_DOMAIN,
                            VNC_VCENTER_DEFAULT_SG, VNC_VCENTER_DEFAULT_SG_FQN,
                            VNC_VCENTER_IPAM, VNC_VCENTER_IPAM_FQN,
                            VNC_VCENTER_PROJECT)
@@ -45,21 +45,24 @@ class VSphereAPIClient(object):
 
 
 class ESXiAPIClient(VSphereAPIClient):
-    _version = ''
-
     def __init__(self, esxi_cfg):
         super(ESXiAPIClient, self).__init__()
+        self._esxi_cfg = esxi_cfg
+        self._create_connection()
+
+    def _create_connection(self):
         self._si = SmartConnectNoSSL(
-            host=esxi_cfg.get('host'),
-            user=esxi_cfg.get('username'),
-            pwd=esxi_cfg.get('password'),
-            port=esxi_cfg.get('port'),
-            preferredApiVersions=esxi_cfg.get('preferred_api_versions')
+            host=self._esxi_cfg.get('host'),
+            user=self._esxi_cfg.get('username'),
+            pwd=self._esxi_cfg.get('password'),
+            port=self._esxi_cfg.get('port'),
+            preferredApiVersions=self._esxi_cfg.get('preferred_api_versions')
         )
-        self._datacenter = self._si.content.rootFolder.childEntity[0]
         atexit.register(Disconnect, self._si)
+        self._datacenter = self._si.content.rootFolder.childEntity[0]
         self._property_collector = self._si.content.propertyCollector
         self._wait_options = vmodl.query.PropertyCollector.WaitOptions()
+        self._version = ''
 
     def get_all_vms(self):
         return self._datacenter.vmFolder.childEntity
@@ -90,6 +93,9 @@ class ESXiAPIClient(VSphereAPIClient):
         if update_set:
             self._version = update_set.version
         return update_set
+
+    def renew_connection(self):
+        self._create_connection()
 
     def read_vm_properties(self, vmware_vm):
         filter_spec = make_filter_spec(vmware_vm, VM_PROPERTY_FILTERS)
@@ -352,16 +358,26 @@ class VNCAPIClient(object):
         logger.info('Virtual Machine %s created in VNC', vnc_vm.name)
 
     def get_all_vms(self):
-        vms = self.vnc_lib.virtual_machines_list().get('virtual-machines')
-        return [self.read_vm(vm['uuid']) for vm in vms]
+        vms_data = self.vnc_lib.virtual_machines_list().get('virtual-machines')
+        vms = []
+        for vm_data in vms_data:
+            vnc_vm = self.read_vm(vm_data['uuid'])
+            if vnc_vm.id_perms.creator == ID_PERMS_CREATOR:
+                vms.append(vnc_vm)
+        return vms
 
     def get_all_vm_uuids(self):
-        vms = self.vnc_lib.virtual_machines_list().get('virtual-machines')
-        return [vm['uuid'] for vm in vms]
+        vms_data = self.vnc_lib.virtual_machines_list().get('virtual-machines')
+        vm_uuids = []
+        for vm_data in vms_data:
+            vnc_vm = self.read_vm(vm_data['uuid'])
+            if vnc_vm.id_perms.creator == ID_PERMS_CREATOR:
+                vm_uuids.append(vm_data['uuid'])
+        return vm_uuids
 
     def get_vmi_uuids_by_vm_uuid(self, vm_uuid):
         vm = self.read_vm(vm_uuid)
-        return [vmi_ref['uuid'] for vmi_ref in vm.get_virtual_machine_interface_back_refs()]
+        return [vmi_ref['uuid'] for vmi_ref in vm.get_virtual_machine_interface_back_refs() or ()]
 
     def read_vm(self, uuid):
         return self.vnc_lib.virtual_machine_read(id=uuid)
--- tests/functional/test_vm_not_in_contrail.py
+++ tests/functional/test_vm_not_in_contrail.py
@@ -99,7 +99,7 @@ def test_vm_powered_on(database, controller, vcenter_api_client, vnc_api_client,
     vrouter_api_client.add_port.assert_not_called()
 
 
-def test_sync(monitor, database, esxi_api_client, vcenter_api_client, vrouter_api_client, vnc_api_client, vmware_vm_1,
+def test_sync(controller, database, esxi_api_client, vcenter_api_client, vrouter_api_client, vnc_api_client, vmware_vm_1,
               portgroup, vnc_vm):
     esxi_api_client.get_all_vms.return_value = [vmware_vm_1]
     vnc_api_client.read_vn.return_value = None
@@ -107,7 +107,7 @@ def test_sync(monitor, database, esxi_api_client, vcenter_api_client, vrouter_ap
     vcenter_api_client.get_dpg_by_key.return_value = portgroup
     vrouter_api_client.read_port.return_value = None
 
-    monitor.sync()
+    controller.sync()
 
     assert len(database.get_all_vm_models()) == 1
     assert not database.get_all_vn_models()
