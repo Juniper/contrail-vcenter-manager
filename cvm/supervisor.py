36925a07057838afd356d2b9e035c78304b1ea42
Krzysztof Gromadzki <krzysztof.gromadzki@codilime.com>
2018-11-08 16:36:27 +0100

    Add cancelling wait_for_updates() call with connections with ESXi hangs
    Change-Id: Id849723535ad8b6e3d9f0896187244eda6848fa3
    Depends-On: I81f3a102010a8809df3f8ca29636fc6532392e33

---
+++ cvm/supervisor.py
@@ -0,0 +1,48 @@
+import gevent
+import logging
+
+from cvm.constants import SUPERVISOR_TIMEOUT
+
+logger = logging.getLogger(__name__)
+
+
+class Supervisor(object):
+    def __init__(self, event_listener, esxi_api_client):
+        self._event_listener = event_listener
+        self._esxi_api_client = esxi_api_client
+        self._to_supervisor = gevent.queue.Queue()
+        self._greenlet = None
+
+    def supervise(self):
+        self._greenlet = gevent.spawn(self._event_listener.listen, self._to_supervisor)
+        while True:
+            try:
+                self._to_supervisor.get()
+                logger.info('GOT MESSAGE FROM LISTENER GREENLET: BEFORE')
+                self._to_supervisor.get(timeout=SUPERVISOR_TIMEOUT)
+                logger.info('GOT MESSAGE FROM LISTENER GREENLET: AFTER')
+            except gevent.queue.Empty:
+                try:
+                    logger.error('Canceling wait for updates call...')
+                    self._esxi_api_client.cancel_wait_for_updates()
+                    self._to_supervisor.get()
+                    logger.error('Cancelled wait for updates call')
+                except Exception:
+                    logger.error('Failed to cancel wait for updates')
+                    logger.info('Renewing connection to ESXi...')
+                    self._renew_esxi_connection_retry()
+                    logger.info('Renewed connection to ESXi')
+                    logger.info('Respawing event handling greenlet')
+                    self._greenlet.kill(block=False)
+                    self._greenlet = gevent.spawn(self._event_listener.listen, self._to_supervisor)
+                    logger.info('Respawned event handling greenlet')
+
+    def _renew_esxi_connection_retry(self):
+        while True:
+            try:
+                self._esxi_api_client.renew_connection()
+                break
+            except Exception:
+                logger.error('Error during renewing connection to ESXi')
+                gevent.sleep(5)
+                logger.error('Retrying to renew connection to ESXi...')
