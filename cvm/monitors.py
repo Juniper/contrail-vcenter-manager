22e31d5d62f0603e60135ef2d5ce50b78093086f
Krzysztof Gromadzki <krzysztof.gromadzki@codilime.com>
2018-11-08 16:36:27 +0100

    Add cancelling wait_for_updates() call with connections with ESXi hangs
    Change-Id: Id849723535ad8b6e3d9f0896187244eda6848fa3

--- cvm/monitors.py
+++ cvm/monitors.py
@@ -1,3 +1,8 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
 class VMwareMonitor(object):
     def __init__(self, esxi_api_client, vmware_controller):
         self._esxi_api_client = esxi_api_client
@@ -6,8 +11,16 @@ class VMwareMonitor(object):
     def sync(self):
         self._controller.sync()
 
-    def start(self):
+    def start(self, to_supervisor, from_supervisor):
+        logger.info('Event handling greenlet start working')
         while True:
+            from_supervisor.get()
+            logger.info('Get permission for wait for updates')
             update_set = self._esxi_api_client.wait_for_updates()
+            logger.info('Got update set from ESXi')
+            to_supervisor.put('AFTER_WAIT_FOR_UPDATES')
             if update_set:
+                logger.info('Not empty update set')
                 self._controller.handle_update(update_set)
+            else:
+                logger.info('Empty update set')
