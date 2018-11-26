a7d7cb89e703b2e279f46fe7abfec2ed702e1870
Krzysztof Gromadzki <krzysztof.gromadzki@codilime.com>
2018-11-26 14:43:55 +0100

    Split reading and handling events into two greenlets
    Change-Id: I81f3a102010a8809df3f8ca29636fc6532392e33

--- cvm/monitors.py
+++ cvm/monitors.py
@@ -1,3 +1,8 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
 class VMwareMonitor(object):
     def __init__(self, esxi_api_client, vmware_controller):
         self._esxi_api_client = esxi_api_client
@@ -6,8 +11,9 @@ class VMwareMonitor(object):
     def sync(self):
         self._controller.sync()
 
-    def start(self):
+    def start(self, update_set_queue):
         while True:
-            update_set = self._esxi_api_client.wait_for_updates()
-            if update_set:
-                self._controller.handle_update(update_set)
+            update_set = update_set_queue.get()
+            logger.info('Starting process update set')
+            self._controller.handle_update(update_set)
+            logger.info('Finished procesing update set')
